aff461057f8bfe3c6663d9a0cf1278cfdc7ee401
PremShakti <premshakti.ps@gmail.com>
2025-03-03 16:20:38 +0530

    i can make it more acurate if have some more time

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 // List of box titles (Only title is given)
 import { data } from "./data.js";
 
-
-const boxData=data.tasks[0].assets
+const boxData = data.tasks[0].assets;
 
 // const boxData = [
 //   "Login Form",
@@ -12,6 +11,21 @@ const boxData=data.tasks[0].assets
 // ];
 
 const formContainer = document.getElementById("formContainer");
+const journeyBoardContainer = document.getElementById(
+  "journey-board-container"
+);
+const rightArrow = document.getElementById("right-arrow");
+
+
+rightArrow.addEventListener("click", () => {
+  if (journeyBoardContainer.style.transform === "translateX(-250px)") {
+    rightArrow.style.transform = "rotate(180deg)";
+    journeyBoardContainer.style.transform = "translateX(-50px)";
+  } else {
+    rightArrow.style.transform = "rotate(0deg)";
+    journeyBoardContainer.style.transform = "translateX(-250px)";
+  }
+});
 
 // Function to create a reusable box and insert the correct form
 function createBox(assetData) {
@@ -56,7 +70,137 @@ function createBox(assetData) {
     <div class='description-content'>
          <p class='description'> <span >Description</span> : ${assetData.asset_description}</p>
         </div>
+  <div class="Threadbuild-box-container">
+      <div class="collaps-bar" id="collaps-bar">
+        <span id="up-icon" class="up-icon"
+          ><i class="fa-solid fa-angle-up"></i
+        ></span>
 
+        <h3>Thread A</h3>
+      </div>
+
+      <div class="form-container">
+        <form action="" class="form">
+          <div>
+            <div class="sub-thread-container">
+              <div class="subthread">
+                <div class="innerinbox">
+                  <div class="iner-subthread-heading">
+                    <p>Sub thread 1</p>
+                  </div>
+
+                  <textarea
+                    name=""
+                    id=""
+                    class="textareainner"
+                    placeholder="Enter Text here"
+                  ></textarea>
+                </div>
+
+                <div class="iner-menue-items">
+                  <i class="fa-solid fa-lightbulb"></i>
+                  <i class="fa-solid fa-message"></i>
+                  <i class="fa-solid fa-clipboard-question"></i>
+                  <i class="fa-brands fa-canadian-maple-leaf"></i>
+                </div>
+              </div>
+
+              <div class="subthread">
+                <div class="innerinbox">
+                  <div class="iner-subthread-heading">
+                    <p>Sub thread 1</p>
+                  </div>
+
+                  <textarea
+                    name=""
+                    id=""
+                    class="textareainner"
+                    placeholder="Enter Text here"
+                  ></textarea>
+                </div>
+
+                <div class="iner-select-container">
+                  <select name="" id="" class="iner-select">
+                    <option value="" class="inner-select-options">
+                      select category
+                    </option>
+                    <option value="" class="inner-select-options">
+                      select 1
+                    </option>
+                    <option value="" class="inner-select-options">
+                      select 2
+                    </option>
+                    <option value="" class="inner-select-options">
+                      select 3
+                    </option>
+                    <option value="" class="inner-select-options">
+                      select 4
+                    </option>
+                  </select>
+                  <select name="" id="" class="iner-select">
+                    <option value="" class="inner-select-options">
+                      select category
+                    </option>
+                    <option value="" class="inner-select-options">
+                      select 1
+                    </option>
+                    <option value="" class="inner-select-options">
+                      select 2
+                    </option>
+                    <option value="" class="inner-select-options">
+                      select 3
+                    </option>
+                    <option value="" class="inner-select-options">
+                      select 4
+                    </option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div>
+              <button type="button" class="sub-thread-button">
+                <span><i class="fa-solid fa-plus"></i></span>
+                <p>Sub-thread</p>
+              </button>
+            </div>
+          </div>
+
+          <div class="">
+            <div class="innerinbox">
+              <div class="iner-subthread-heading">
+                <p>Sub thread 1</p>
+              </div>
+
+              <textarea
+                name=""
+                id=""
+                class="textareainner"
+                placeholder="Enter Text here"
+              ></textarea>
+            </div>
+
+            <div class="iner-select-container">
+              <div><p class="Thread-Creadet">Thread Creadet</p></div>
+              <select name="" id="" class="iner-select">
+                <option value="" class="inner-select-options">
+                  select category
+                </option>
+                <option value="" class="inner-select-options">select 1</option>
+                <option value="" class="inner-select-options">select 2</option>
+                <option value="" class="inner-select-options">select 3</option>
+                <option value="" class="inner-select-options">select 4</option>
+              </select>
+            </div>
+          </div>
+          <div class="newthreadbtn-container">
+            <button type="button" class="sub-thread-button">
+              <span><i class="fa-solid fa-plus"></i></span>
+              <p>New Thread</p>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
       </div>
         `;
   } else if (assetData.asset_title === "Structure you pointers") {
@@ -152,5 +296,19 @@ function createBox(assetData) {
 
 // Generate boxes dynamically
 boxData.forEach(createBox);
+
+const upIcon = document.getElementById("up-icon");
+const collapsBar = document.getElementById("collaps-bar");
+const formContainerThreadBuild = document.querySelector(".form-container");
+
+collapsBar.addEventListener("click", () => {
+  if (formContainerThreadBuild.style.height === "0px") {
+    upIcon.style.transform = "rotate(180deg)";
+    formContainerThreadBuild.style.height = "400px";
+  } else {
+    formContainerThreadBuild.style.height = "0px";
+    upIcon.style.transform = "rotate(0deg)";
+  }
+});
 
 //REACH TEXT EDITOR
